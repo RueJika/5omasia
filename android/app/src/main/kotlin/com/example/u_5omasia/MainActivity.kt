7bd9a2221db70477d6c592e4e5fdf8fad99a4620
Rogina2003 <roginakamel2003@gmail.com>
2025-03-09 19:52:56 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/u_5omasia/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.u_5omasia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Khomasia());
+}
+
+class Khomasia extends StatelessWidget {
+  const Khomasia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
